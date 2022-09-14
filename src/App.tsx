b368e5c382fad9f4a25d6aa25a906f493d2a9e28
i1-kmv <i1-kmv@mail.ru>
2022-09-14 15:27:51 +0300

    create and configure themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Route, Routes} from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>Страница эбаут</Link>
             <Suspense fallback={<div>Loading...</div>}>
