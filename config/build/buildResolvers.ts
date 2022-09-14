25c1f8fbf7dea2956718c6c6758e635a33cfa360
i1-kmv <i1-kmv@mail.ru>
2022-09-14 18:18:21 +0300

    config absolute imports, change project architecture

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
 
-export const buildResolvers = (): ResolveOptions => {
+export const buildResolvers = (options: BuildOptions): ResolveOptions => {
   return {
       extensions: ['.tsx', '.ts', '.js'],
+      preferAbsolute: true, //работа с абсолютными путями
+      modules: [options.paths.src, 'node_modules'],
+      alias:{}, //спецсимвол, который указывается перед абсолютным путем, пустым объектом упраздняем
+      mainFiles: ['index'], //задаем главный файл для каждого модуля
   }
 }
