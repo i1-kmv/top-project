efbdbc30bc8b043a0e09270a8dffbbbdcf1f8f28
i1-kmv <i1-kmv@mail.ru>
2022-09-19 12:36:57 +0300

    config routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
-import {Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -17,12 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>Страница эбаут</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
