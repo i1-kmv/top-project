4bd49eaf718f8fae13099cc1ae986a1b1aea0e46
i1-kmv <i1-kmv@mail.ru>
2022-09-20 08:54:21 +0300

    create NavBar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
@@ -11,11 +11,10 @@ const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={classNames('app', {},[theme] )}>
-            <button onClick={toggleTheme}>Toggle theme</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>Страница эбаут</Link>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>Toggle theme</button>
         </div>
     );
 };
