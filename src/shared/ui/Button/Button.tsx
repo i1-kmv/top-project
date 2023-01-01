cf6316822d5be1aaddf72815941422f962308af4
Iarovoi Ivan <i1-kmv@mail.ru>
2023-01-01 18:12:11 +0300

    add RTL

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,9 +1,10 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./Button.module.scss"
-import {ButtonHTMLAttributes, FC} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
+// @ts-ignore
+import cls from './Button.module.scss';
 
 export enum ThemeButton {
-    CLEAR = "clear",
+    CLEAR = 'clear',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -12,15 +13,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-
     const {
         className, children, theme, ...otherProps
-    } = props
-
+    } = props;
 
     return (
-        <button className={classNames(cls.Button, {[cls[theme]]: true}, [className])} {...otherProps}>
+        <button className={classNames(cls.Button, { [cls[theme]]: true }, [className])} {...otherProps}>
             {children}
         </button>
-    )
-}
+    );
+};
